d159b10fc449f5bb631a44908bb2d67efe33b383
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2017-03-29 22:32:18 -0500

    Initial Upload

---
+++ src/main/java/com/crypto/currency/ScroogeCoinApplication.java
@@ -0,0 +1,12 @@
+package com.crypto.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScroogeCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScroogeCoinApplication.class, args);
+	}
+}
